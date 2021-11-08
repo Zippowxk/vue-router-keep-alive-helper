0f283ddf304994359e7ffd7a21cd1ff36dfe986a
王鑫凯 <wangxinkai@haier.com>
2021-11-08 09:34:33 +0800

    fix: #24

--- src/extension.js
+++ src/extension.js
@@ -6,13 +6,13 @@ export function extendHistory(history) {
     const old = Object.assign({}, history.state);
     const s = Object.assign(old, state);
     rstmp.call(history, s, op, path);
-  }
+  };
   const historyPushState = history.pushState;
   history.pushState = function (state, op, path) {
     const old = Object.assign({}, history.state);
     const s = Object.assign(old, state);
     historyPushState.call(history, s, op, path);
-  }
+  };
 }
 
 export function extendVue(Vue) {
@@ -20,7 +20,7 @@ export function extendVue(Vue) {
   /**
    * @description remove the cache in <keep-alive> component before invoke $destroy
    */
-  Vue.prototype.$keepAliveDestroy = function () {
+  Vue.prototype.$keepAliveDestroy = function (vmCurrent) {
     if (this.$vnode && this.$vnode.data.keepAlive) {
       if (
         this.$vnode.parent &&
@@ -43,7 +43,27 @@ export function extendVue(Vue) {
                 keys.splice(index, 1);
               }
             }
-            delete cache[key];
+            cache[key] = null;
+            // fix memory leaks
+            if (
+              this.$vnode.parent.componentOptions.children &&
+              Array.isArray(this.$vnode.parent.componentOptions.children)
+            ) {
+              this.$vnode.parent.componentOptions.children.length = 0
+            }
+            if (cache[vmCurrent.$vnode.key]){
+              cache[vmCurrent.$vnode.key].parent.componentOptions.children = [cache[vmCurrent.$vnode.key]]
+              cache[vmCurrent.$vnode.key].parent.elm = cache[vmCurrent.$vnode.key].parent.componentInstance.$el
+            }
+            if (
+              this.$parent.$children &&
+              Array.isArray(this.$parent.$children)
+            ) {
+              const index = this.$parent.$children.indexOf(this);
+              if (index >= 0) {
+                this.$parent.$children.splice(index, 1);
+              }
+            }
           }
         }
       }
